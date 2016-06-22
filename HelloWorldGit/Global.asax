676d4bbab664defe13893f9d701feb983114f235
krunal shah <krunals@sfwltd.co.uk>
2016-06-22 18:57:26 +0530

    Publish the code

---
+++ HelloWorldGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloWorldGit.Global" Language="C#" %>
---
+++ HelloWorldGit/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWorldGit.Startup))]
+namespace HelloWorldGit
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
